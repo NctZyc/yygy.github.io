0e608825f493eaaedbecb81545420c7eab6f3256
NctZyc <2058443293@qq.com>
2023-09-27 10:12:04 +0800

    添加右键菜单项 休息一下

--- style/js/contextMenuAction.js
+++ style/js/contextMenuAction.js
@@ -25,6 +25,17 @@
 						case "theme":				
 							alert("主题还没设计好，哪位有艺术细胞的小伙伴给些建议呀~");
 						break;
+						case "rest":		
+							if($(".main-content").css("visibility") == "visible"){
+								$(".main-content").css("visibility", "hidden");
+								$(".context-menu-icon-rest").addClass("context-menu-icon-rest-try");
+								$(".context-menu-icon-rest span")[0].innerText="继续奋斗";
+							}else{
+								$(".main-content").css("visibility", "visible");
+								$(".context-menu-icon-rest").removeClass("context-menu-icon-rest-try");
+								$(".context-menu-icon-rest span")[0].innerText="休息一下";
+							}
+						break;
 						case "mode":							
 							var mode = $("html").attr("mode");
 							 if(mode=="dark"){
@@ -52,6 +63,7 @@
 					"home": { name: "欢迎回家", icon: "home" },
 					"theme": {name: "切换主题", icon: "theme"},
 					"mode": {name: "暗黑模式", icon: 'mode'},
+					"rest": {name: "休息一下", icon: 'rest'},
                     "issue": {name: "常见问题", icon: 'issue'}
                 }
             });
